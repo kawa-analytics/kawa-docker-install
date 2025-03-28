99cb45e61c32a9f013eac677107e2f3ffeda89cd
emmanuel <emmanuel@kawa-analytics.com>
2025-03-28 12:07:58 -0400

    remove max_table_size_to_drop limit

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ if [[ -n "$ENV_FILE" ]]; then
     fi
 fi
 
-kawa_latest_version="1.29.x"
+kawa_latest_version="1.30.x"
 
 if [[ -z "$KAWA_BRANCH_NAME" ]]; then
     if [[ "$interactive" == "true" ]]; then
